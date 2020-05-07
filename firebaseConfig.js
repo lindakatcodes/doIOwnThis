bea88c42f466b24388d1d8d3c0b7aeac08954ee3
Lindakat <lindakt16@gmail.com>
2020-05-07 17:30:58 -0500

    Removed Dexie; added Firebase setup

---
+++ firebaseConfig.js
@@ -0,0 +1,44 @@
+import * as firebase from 'firebase/app'
+import 'firebase/analytics'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+import 'firebase/performance'
+
+// firebase init goes here
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: 'doiownthis.firebaseapp.com',
+  databaseURL: 'https://doiownthis.firebaseio.com',
+  projectId: 'doiownthis',
+  storageBucket: 'doiownthis.appspot.com',
+  messagingSenderId: process.env.MESSAGING_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+}
+firebase.initializeApp(config)
+
+// firebase utils
+const db = firebase.firestore()
+const storage = firebase.storage()
+const perf = firebase.performance()
+const auth = firebase.auth()
+const currentUser = auth.currentUser
+
+// date issue fix according to firebase
+const settings = {
+  timestampsInSnapshots: true
+}
+db.settings(settings)
+
+// firebase collections
+const allItems = db.collection('all-items')
+
+export {
+  db,
+  storage,
+  perf,
+  auth,
+  currentUser,
+  allItems
+}
