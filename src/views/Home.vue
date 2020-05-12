f519b77a641dfc163bacc2036a68e01318cacce0
Lindakat <lindakt16@gmail.com>
2020-05-12 00:51:23 -0500

    Added transition effect on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="home">
     <FilterBar />
-    <div v-if="!signedIn" class="intro">
-      <h2>Ready to keep track of your personal items while out and about?</h2>
-      <p>
-        Ever run into this problem - you're out at the store, browsing your favorite items. But then you stop to wonder
-        - "Do I already own this one?" Or maybe it's "Do I have enough purples?"
-      </p>
+    <transition name="slide-fade" mode="out-in">
+      <div v-if="!signedIn" key="signed-out" class="intro">
+        <h2>Ready to keep track of your personal items while out and about?</h2>
+        <p>
+          Ever run into this problem - you're out at the store, browsing your favorite items. But then you stop to
+          wonder - "Do I already own this one?" Or maybe it's "Do I have enough purples?"
+        </p>
 
-      <p>
-        Well wonder no longer! Now, you can keep track of the items you own for your favorite collections, and easily
-        verify if you've already got that shade or style!
-      </p>
+        <p>
+          Well wonder no longer! Now, you can keep track of the items you own for your favorite collections, and easily
+          verify if you've already got that shade or style!
+        </p>
 
-      <p>
-        Just sign in above, and be on your way to making your life easier!
-      </p>
-    </div>
-    <div v-else>
-      <router-link :to="{ name: 'add-new' }" class="add-new-link">Got something new? Add it here!</router-link>
-    </div>
+        <p>
+          Just sign in above, and be on your way to making your life easier!
+        </p>
+      </div>
+      <div v-else key="signed-in">
+        <router-link :to="{ name: 'add-new' }" class="add-new-link">Got something new? Add it here!</router-link>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -50,5 +52,17 @@
     font-weight: 600;
     margin-left: 15%;
     margin-top: 5%;
+  }
+
+  .slide-fade-leave-active {
+    transition: all 0.3s cubic-bezier(0.33, 1, 0.68, 1);
+  }
+  .slide-fade-enter-active {
+    transition: all 0.8s cubic-bezier(0.61, 1.59, 0.74, 0.85);
+  }
+  .slide-fade-enter,
+  .slide-fade-leave-to {
+    transform: translateX(10px);
+    opacity: 0;
   }
 </style>
