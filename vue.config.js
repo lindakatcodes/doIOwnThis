9ba96a79446238c0ba8ffd546bd6b902a8a55b4a
Lindakat <lindakt16@gmail.com>
2020-06-15 23:10:40 -0500

    Same ol same ol

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   chainWebpack: (config) => {
-    config.module.rule('eslint').exclude.add(/dist/).end();
+    config.module.rule('eslint').exclude.add('E:\\ProgrammingFiles\\diot-project\\dist').end();
   },
 };
